b89b51cb53eeaeb62646928adc52c52b4c7f6708
Hugo José Bello Gutiérrez <hugo.bello.gu@gmail.com>
2018-07-17 18:32:09 +0200

    adapted to classes

--- src/examples/consumer_example.py
+++ src/examples/consumer_example.py
@@ -5,32 +5,32 @@ sys.path.append("..")
 from test_bed_options import TestBedOptions
 from test_bed_adapter import TestBedAdapter
 
+class ConsumerExample:
+    def main(self):
+        options = {
+           "auto_register_schemas": False,
+           # "kafka_host": 'driver-testbed.eu:3501',
+           # "schema_registry": 'http://driver-testbed.eu:3502',
+           "kafka_host": '127.0.0.1:3501',
+           "schema_registry": 'http://localhost:3502',
+           "fetch_all_versions": False,
+           "from_off_set": True,
+           "client_id": 'PYTHON TEST BED ADAPTER',
+           "consume": ["standard_cap"]}
 
-def consumer_example():
-    options = {
-       "auto_register_schemas": False,
-       # "kafka_host": 'driver-testbed.eu:3501',
-       # "schema_registry": 'http://driver-testbed.eu:3502',
-       "kafka_host": '127.0.0.1:3501',
-       "schema_registry": 'http://localhost:3502',
-       "fetch_all_versions": False,
-       "from_off_set": True,
-       "client_id": 'PYTHON TEST BED ADAPTER',
-       "consume": ["standard_cap"]}
+        test_bed_options = TestBedOptions(options)
+        test_bed_adapter = TestBedAdapter(test_bed_options)
 
-    test_bed_options = TestBedOptions(options)
-    test_bed_adapter = TestBedAdapter(test_bed_options)
+        # This funcion will act as a handler. It only prints the incoming messages
+        handle_message = lambda message: logging.info("\n\n-----\nIncoming message\n-----\n\n" + str(message))
 
-    # This funcion will act as a handler. It only prints the incoming messages
-    handle_message = lambda message: logging.info("\n\n-----\nIncoming message\n-----\n\n" + str(message))
+        # Here we add the message to the test bed adapter
+        test_bed_adapter.on_message += handle_message
 
-    # Here we add the message to the test bed adapter
-    test_bed_adapter.on_message += handle_message
-
-    # We initialize the process (catching schemas and so on) and we listen the messages from the topic standard_cap
-    test_bed_adapter.initialize()
-    test_bed_adapter.kafka_managers["standard_cap"].listen_messages()
+        # We initialize the process (catching schemas and so on) and we listen the messages from the topic standard_cap
+        test_bed_adapter.initialize()
+        test_bed_adapter.kafka_managers["standard_cap"].listen_messages()
 
 
 if __name__ == '__main__':
-    consumer_example()
+    ConsumerExample().main()
