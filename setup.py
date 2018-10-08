738585c3d2d20a13c2d6064442b2a860140eae4b
hugo.bello.gu@gmail.com <hugo.bello.gu@gmail.com>
2018-10-08 11:43:56 +0200

    fixed problem in kafka submodule importations

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-test-bed-adapter",
-    version="0.0.2",
+    version="0.0.3",
     author="Hugo J. Bello",
     author_email="hjbello.wk@gmail.com",
     description="This is the test-bed adapter for Python: it allows you to easily connect Python services to the Apache Kafka test-bed via Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DRIVER-EU/python-test-bed-adapter",
+    include_package_data=True,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
---
+++ test_bed_adapter/kafka/__init__.py
@@ -0,0 +1,1 @@
+from . import consumer_manager, heartbeat_manager, kafka_manager, producer_manager
